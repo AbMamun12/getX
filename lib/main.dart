516d7713e1782b3a723bee3913f0ca8031c73fa2
Abdullah <abdullah.mamun8191@gmail.com>
2025-07-16 22:36:36 +0600

    Obs ,Obx, GetxController, GetBuilder

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:getx/counter_controller.dart';
+import 'package:getx/home_screen.dart';
+
+void main(){
+  runApp(CounterApp());
+}
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  GetMaterialApp(
+      home: const HomeScreen(),
+      initialBinding: ControllerBinder(),
+    );
+  }
+}
+
+class ControllerBinder extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(CounterController());
+  }
+
+}
